f6cc5e7c245b2b58aea64a6d426bf6637d8b6ac4
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-06-10 12:17:05 +0300

    main page created

--- src/main/java/com/mustafaoguz/cng457festivalsdesktopapp/MainController.java
+++ src/main/java/com/mustafaoguz/cng457festivalsdesktopapp/MainController.java
@@ -3,7 +3,7 @@ package com.mustafaoguz.cng457festivalsdesktopapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
