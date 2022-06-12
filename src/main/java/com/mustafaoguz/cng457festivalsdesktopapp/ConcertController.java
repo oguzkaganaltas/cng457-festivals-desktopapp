2ce5502ec85398d8254c13c49c85020c7ce91a6d
Dexess <aygunmustafa999@gmail.com>
2022-06-12 10:59:16 +0300

    combobox and initialize implemented

--- src/main/java/com/mustafaoguz/cng457festivalsdesktopapp/ConcertController.java
+++ src/main/java/com/mustafaoguz/cng457festivalsdesktopapp/ConcertController.java
@@ -30,9 +30,42 @@ public class ConcertController {
     private DatePicker datePickerConcert;
     @FXML
     private ComboBox festivalComboBox;
+    @FXML
+    private ComboBox festivalRunComboBox;
+    @FXML
+    private ComboBox performerComboBox;
 
     public void addButtonPressed(ActionEvent event) throws IOException {
 
+    }
+
+    public void isFestivalSelected(ActionEvent event) throws IOException, org.json.simple.parser.ParseException {
+        StringBuilder response = new StringBuilder();
+        String selectedItem = festivalComboBox.getValue().toString();
+        String festivalRunId = selectedItem.split("-")[0];
+        HttpURLConnection connection = (HttpURLConnection)new URL("http://localhost:8080/getallfestivalruns/"+festivalRunId).openConnection();
+
+        connection.setRequestMethod("GET");
+        int responsecode = connection.getResponseCode();
+        if(responsecode == 200){
+            Scanner scanner = new Scanner(connection.getInputStream());
+            while(scanner.hasNextLine()){
+                response.append(scanner.nextLine());
+            }
+            scanner.close();
+        }
+
+        JSONParser parser = new JSONParser();
+        JSONArray array = (JSONArray) parser.parse(response.toString());
+        for (Object o : array) {
+            try {
+                JSONObject temp = (JSONObject) o;
+                festivalRunComboBox.getItems().add(temp.get("festRunId")+"-"+temp.get("date"));
+                System.out.println(temp.get("festRunId")+"-"+temp.get("date"));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public void initialize() throws IOException, ParseException, org.json.simple.parser.ParseException {
@@ -59,6 +92,33 @@ public class ConcertController {
                 System.out.println(e.getMessage());
             }
         }
+
+        StringBuilder performerResponse = new StringBuilder();
+        HttpURLConnection performerConnection = (HttpURLConnection)new URL("http://localhost:8080/getallperformers").openConnection();
+        performerConnection.setRequestMethod("GET");
+        int performerResponseCode = performerConnection.getResponseCode();
+        if(performerResponseCode == 200){
+            Scanner scanner = new Scanner(performerConnection.getInputStream());
+            while(scanner.hasNextLine()){
+                performerResponse.append(scanner.nextLine());
+            }
+            scanner.close();
+        }
+
+        JSONParser performerParser = new JSONParser();
+        JSONArray performerArray = (JSONArray) performerParser.parse(performerResponse.toString());
+
+        for (Object p : performerArray) {
+            try {
+                JSONObject tempPerformer = (JSONObject) p;
+                performerComboBox.getItems().add(tempPerformer.get("performerId") + "-" + tempPerformer.get("name")+" " + tempPerformer.get("surname"));
+                System.out.println(tempPerformer.get("performerId") + "-" + tempPerformer.get("name")+" " + tempPerformer.get("surname"));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
     }
 
 }
